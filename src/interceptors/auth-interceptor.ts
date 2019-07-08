1927424a0a2cc2bcf5e6e3d2fa700ba883fb4f83
Leonardo Batista Carias <leonardo1carias@gmail.com>
2019-07-08 20:40:52 -0300

    Povoando dinamicamente estados e cidades

--- src/interceptors/auth-interceptor.ts
+++ src/interceptors/auth-interceptor.ts
@@ -8,23 +8,22 @@ import { API_CONFIG } from '../config/api.config';
 export class AuthInterceptor implements HttpInterceptor {
 
     constructor(public storage: StorageService) {
-
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        
+
         let localUser = this.storage.getLocalUser();
 
         let N = API_CONFIG.baseUrl.length;
-        let requestToApi = req.url.substring(0, N) == API_CONFIG.baseUrl;
+        let requestToAPI = req.url.substring(0, N) == API_CONFIG.baseUrl;
 
-        if(localUser && requestToApi) {
-            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});    
-            return next.handle(authReq);        
+        if (localUser && requestToAPI) {
+            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
+            return next.handle(authReq);
         }
         else {
-            return next.handle(req);        
-        }        
+            return next.handle(req);
+        }
     }
 }
 
