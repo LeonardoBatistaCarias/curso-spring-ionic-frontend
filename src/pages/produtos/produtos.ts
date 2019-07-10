bfe3228ebca3d46bef0c9e220055af0eb0c9036a
Leonardo Batista Carias <leonardo1carias@gmail.com>
2019-07-10 19:59:04 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -16,22 +16,27 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public produtoService : ProdutoService) {
+    public produtoService : ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-    let categoriaId = this.navParams.get('categoria_id')
+    let categoriaId = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoriaId)
     .subscribe( response => {
       this.items = response['content'];
+      loader.dismiss();
       this.loadImageUrls();
     },
-    error => {});
+    error => {
+      loader.dismiss();
+    });
   };
 
   loadImageUrls() {
     for (var i=0; i<this.items.length; i++) {
-      let item = this.items[i];
+      let item = this.items[i];      
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
           item.imageUrl = `${API_CONFIG.bucketUrl}/prod${item.id}-small.jpg`;
@@ -43,4 +48,13 @@ export class ProdutosPage {
   showDetail(produto_id : string){
     this.navCtrl.push("ProdutoDetailPage", {produto_id : produto_id});
   }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+  
 }
